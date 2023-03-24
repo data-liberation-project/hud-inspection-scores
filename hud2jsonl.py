0b5fbd6bd63de2194458c72615e2bc54dcc70113
Saul Pwanson <code@saul.pw>
2023-03-23 21:45:09 -0700

    [refactor] into load_xls and process_hud

--- hud2jsonl.py
+++ hud2jsonl.py
@@ -4,40 +4,53 @@ import os
 import sys
 import json
 import datetime
+import copy
 
 import xlrd
 
 
-def parse_hud_xls(fn):
-    '''
-        Parse HUD inspection data from .xls file at `fn`.  Yield one dictionary per inspection.
-    '''
-    def stderr(*args):
-        print(fn, *args, file=sys.stderr)
+def stderr(*args):
+    print(fn, *args, file=sys.stderr)
 
-    def convert_cell(c):
-        'Return properly-typed value for xls Cell `c`.'
-        if c.ctype == xlrd.XL_CELL_NUMBER:
-            v = int(c.value)
-            if v != c.value:
-                return c.value
-            return v
 
-        if c.ctype == xlrd.XL_CELL_DATE:
-            t = xlrd.xldate.xldate_as_tuple(c.value, 0)
-            dt = datetime.datetime(*t)
-            return dt.isoformat(sep=' ')
+def convert_cell(c):
+    'Return properly-typed value for xls Cell `c`.'
+    if c.ctype == xlrd.XL_CELL_NUMBER:
+        v = int(c.value)
+        if v != c.value:
+            return c.value
+        return v
 
-        if c.ctype == xlrd.XL_CELL_BOOLEAN:
-            return bool(c.value)
+    if c.ctype == xlrd.XL_CELL_DATE:
+        t = xlrd.xldate.xldate_as_tuple(c.value, 0)
+        dt = datetime.datetime(*t)
+        return dt.isoformat(sep=' ')
 
-        if c.ctype == xlrd.XL_CELL_ERROR:
-            stderr(f'error: {c.value}')
-            return f'error: {c.value}'
+    if c.ctype == xlrd.XL_CELL_BOOLEAN:
+        return bool(c.value)
 
-        return c.value.strip()
+    if c.ctype == xlrd.XL_CELL_ERROR:
+        stderr(f'error: {c.value}')
+        return f'error: {c.value}'
 
-    hud_cols = {
+    return c.value.strip()
+
+
+def load_xls(fn, sheetnum=0):
+    'Parse .xls file at `fn`.  Yield one row per data row in the sheet.'
+
+    wb = xlrd.open_workbook(fn, logfile=sys.stderr)
+
+    sheet = wb[sheetnum]
+
+    hdrs = [c.value.strip() for c in sheet.row(0)]
+
+    for i in range(1, sheet.nrows):
+        yield dict(zip(hdrs, [convert_cell(c) for c in sheet.row(i)]))
+
+
+# mapping of source .xls header to cleaned column name
+hud_cols = {
         'REMS Property Id'          : 'rems_property_id',
         'Property_Id'               : 'rems_property_id',
         'Property Name'             : 'property_name',
@@ -47,35 +60,29 @@ def parse_hud_xls(fn):
         'has_active_assistance_ind' : 'has_assistance',
     }
 
-    unused_cols = set([
+unused_cols = set([
         'state_name_text'
-    ])
+])
 
-    wb = xlrd.open_workbook(fn, logfile=sys.stderr)
-    if wb.nsheets > 1:
-        stderr(f'unexpected sheets in {fn}')
 
-    sheet = wb[0]
+def process_hud(row):
+    'Parse HUD data.  Yield one row per inspection.'
+    info = {v:row.pop(k) for k, v in hud_cols.items() if k in row}
 
-    hdrs = [c.value.strip() for c in sheet.row(0)]
+    for i in [1,2,3]:
+        r = copy.copy(info)
+        r['inspection_id'] = row.pop(f'Inspection Id {i}')
+        r['inspection_date'] = row.pop(f'Release Date {i}')
+        r['inspection_score'] = row.pop(f'Inspection Score{i}')
+        if r['inspection_id'] or r['inspection_date'] or r['inspection_score']:
+            yield r
 
-    for i in range(1, sheet.nrows):
-        row = dict(zip(hdrs, [convert_cell(c) for c in sheet.row(i)]))
-
-        d = {v:row.pop(k) for k, v in hud_cols.items() if k in row}
-
-        for i in [1,2,3]:
-            d['inspection_id'] = row.pop(f'Inspection Id {i}')
-            d['inspection_date'] = row.pop(f'Release Date {i}')
-            d['inspection_score'] = row.pop(f'Inspection Score{i}')
-            if d['inspection_id'] or d['inspection_date'] or d['inspection_score']:
-                yield d
-
-        remaining = set(row.keys() - unused_cols)
-        if remaining:
-            stderr(f'unexpected columns remain: {remaining}')
+    remaining = set(row.keys() - unused_cols)
+    if remaining:
+        stderr(f'unexpected columns remain: {remaining}')
 
 
 if __name__ == '__main__':
-    for d in parse_hud_xls(sys.argv[1]):
-        print(json.dumps(d))
+    for row in load_xls(sys.argv[1]):
+        for inspection in process_hud(row):
+            print(json.dumps(inspection))
