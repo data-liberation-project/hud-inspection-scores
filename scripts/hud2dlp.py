a61e7775b408a996df19dbe220e6a489c9296e29
Saul Pwanson <code@saul.pw>
2023-03-28 21:52:42 -0700

    some cleanup

--- scripts/hud2dlp.py
+++ scripts/hud2dlp.py
@@ -2,10 +2,13 @@
 
 import sys
 import copy
+from collections import defaultdict
 
 import dateutil.parser
 
 from utils import JsonOutputter, load, stderr
+
+opt_keep_diffs = False  # set to True to make it easier when tracking down discrepancies
 
 
 def parse_date(s):
@@ -16,6 +19,55 @@ def parse_date(s):
 
 def col_rename(origcolname, newcolname=None):
     return origcolname, newcolname or origcolname
+
+properties = defaultdict(dict)  # property_id:str -> dict of property attributes
+inspections = defaultdict(dict)  # (property_id:str, date:YYYY-MM-DD) -> dict of inspection attributes
+
+def getonly(d, k):
+    n = len(d[k])
+    if n == 1:
+        return list(d[k].values())[0]
+    raise Exception(f'{n} values for {k}')
+
+
+def singlify(d, keep_diffs=opt_keep_diffs):
+    r = {}
+    for k, v in d.items():
+        if not keep_diffs or len(v) == 1:
+            v = list(v.values())[-1]
+        r[k] = v
+    return r
+
+
+def stringify(x):
+    if x is None:
+        return ''
+    return str(x)
+
+
+def dedup(table, key, row, origin=''):
+    'Set table[key]=row, ignoring exact duplicates but warning about differences.'
+
+    d = table[key]
+
+    for k, v in row.items():
+        if not v:
+            continue
+
+        if k not in d:
+            d[k] = {}  # origin -> value
+
+        if d[k]:
+            oldvalues = list(str(x) for x in d[k].values())
+
+            if str(v).lower() not in tuple(x.lower() for x in oldvalues):
+                yield dict(origin=origin, attr=k, oldvalue=oldvalues[-1], newvalue=v, **row)
+            else:
+                continue  # an existing value matches
+
+        d[k][origin] = v
+
+
 
 
 # mapping of source .xls header to cleaned column name
@@ -33,59 +85,17 @@ rems_unused_cols = set([
         'state_name_text'
 ])
 
-properties = { }  # property_id:str -> dict of property attributes
-inspections = { }  # (property_id:str, date:YYYY-MM-DD) -> dict of inspection attributes
-
-def getonly(d, k):
-    n = len(d[k])
-    if n == 1:
-        return list(d[k].values())[0]
-    raise Exception(f'{n} values for {k}')
-
-
-def singlify(d, keep_diffs=True):
-    r = {}
-    for k, v in d.items():
-        if not keep_diffs or len(v) == 1:
-            v = list(v.values())[-1]
-        r[k] = v
-    return r
-
-
-def dedup(table, key, row, origin=''):
-    'Set table[key]=row, ignoring exact duplicates but warning about differences.'
-    if key not in table:
-        table[key] = {}
-
-    d = table[key]
-
-    for k, v in row.items():
-        if not v:
-            continue
-
-        if k not in d:
-            d[k] = {}  # origin -> value
-
-        if d[k]:
-            oldvalues = list(str(x) for x in d[k].values())
-
-            if str(v).lower() not in tuple(x.lower() for x in oldvalues):
-                yield dict(origin=origin, key=key, attr=k, oldvalues=oldvalues, newvalue=v)
-            else:
-                continue  # an existing value matches
-
-        d[k][origin] = v
-
-
 def process_rems(row):
     'Parse HUD data.  Yield one row per inspection.'
     prop = {v:row.pop(k) for k, v in rems_cols.items() if k in row}
-    prop['property_id'] = str(prop['property_id'])
+
+    prop['property_id'] = stringify(prop['property_id'])
 
     yield 'properties', prop
 
     for i in [1,2,3]:
-        r = copy.copy(prop)
+        r = {}
+        r['property_id'] = prop['property_id']
         r['inspection_id'] = row.pop(f'inspection_id_{i}')
         r['date'] = parse_date(row.pop(f'release_date_{i}'))
         r['score'] = row.pop(f'inspection_score{i}')
@@ -123,7 +133,7 @@ pis_unused_cols = set('cbsa_code state_code county_code pha_code fips_state_code
 
 def process_hudpis(row):
     prop = {v:row.pop(k) for k, v in pis_cols.items() if k in row}
-    prop['property_id'] = str(prop['property_id'])
+    prop['property_id'] = stringify(prop['property_id'])
 
     yield 'properties', prop
     yield 'inspections', dict(
@@ -158,7 +168,7 @@ def main(*filenames):
         for dbname, row in load(fn):
             try:
                 for tblname, outputrow in processfunc(row):
-                    propid = str(outputrow.get('property_id'))
+                    propid = stringify(outputrow.get('property_id'))
                     if tblname == 'properties':
                         for duprow in dedup(properties, propid, outputrow, origin=origin):
                             outputter.output('properties-diffs', duprow)
